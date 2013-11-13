1efc3ca6d06a2f8b51ba30439d532f3e85a228dc
kang kyung koo <kyungkoo.kang@yahoo.com>
2013-11-13 16:23:35 +0900

    1차 작업 추가

--- SimpleCamera/src/main/java/com/mc2e/simplecamera/MainActivity.java
+++ SimpleCamera/src/main/java/com/mc2e/simplecamera/MainActivity.java
@@ -1,30 +1,55 @@
 package com.mc2e.simplecamera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import java.io.File;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final int CAPTURE_IMAGE = 0;
+    private static final int CROP_IMAGE_FRONT = 1;
+    private static final int CROP_IMAGE_LEFT = 2;
+    private static final int CROP_IMAGE_RIGHT = 3;
+
+    private Bitmap mFrontBitmap, mLeftBitmap, mRightBitmap;
+
+    private ImageView mFrontView, mLeftView, mRightView;
+    private Button mTakePhotoBtn;
+
+    private Uri mImageCaptureUri;
+
+    // change status
+    //private String mCaptureStatus = "front";
+    private int statusCode = CROP_IMAGE_FRONT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        // init IamgeView
+        mFrontView = (ImageView)findViewById(R.id.front_iv);
+        mLeftView = (ImageView)findViewById(R.id.left_iv);
+        mRightView = (ImageView)findViewById(R.id.right_iv);
 
+        // init button
+        mTakePhotoBtn = (Button)findViewById(R.id.take_photo_btn);
+        mTakePhotoBtn.setOnClickListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,20 +71,98 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    public void onClick(View v) {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+        takePhoto();
     }
 
-}
+    private void takePhoto() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String url = "take_picture_temp.jpg";
+
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        startActivityForResult(intent, CAPTURE_IMAGE);
+    }
+
+    private void cropImage(int status) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(mImageCaptureUri, "image/*");
+
+        intent.putExtra("outputX", 90);
+        intent.putExtra("outputY", 90);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, status);
+    }
+
+    private void saveImage(Intent data) {
+
+        final Bundle extras = data.getExtras();
+
+        if(extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+
+            switch(statusCode) {
+                case CROP_IMAGE_FRONT:
+                    mFrontBitmap = photo;
+                    break;
+
+                case CROP_IMAGE_LEFT:
+                    mLeftBitmap = photo;
+                    break;
+
+                case CROP_IMAGE_RIGHT:
+                    mRightBitmap = photo;
+                    break;
+            }
+        }
+
+        File f = new File(mImageCaptureUri.getPath());
+        if(f.exists())
+            f.delete();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode) {
+
+            case CAPTURE_IMAGE:
+                cropImage(statusCode);
+                break;
+
+            case CROP_IMAGE_FRONT:
+                saveImage(data);
+                Toast.makeText(getApplicationContext(), "image saved.", Toast.LENGTH_SHORT).show();
+                statusCode = CROP_IMAGE_LEFT;
+                takePhoto();
+                break;
+
+            case CROP_IMAGE_LEFT:
+                saveImage(data);
+                Toast.makeText(getApplicationContext(), "image saved.", Toast.LENGTH_SHORT).show();
+                statusCode = CROP_IMAGE_RIGHT;
+                takePhoto();
+                break;
+
+            case CROP_IMAGE_RIGHT:
+                saveImage(data);
+                Toast.makeText(getApplicationContext(), "image saved.", Toast.LENGTH_SHORT).show();
+
+                mFrontView.setImageBitmap(mFrontBitmap);
+                mLeftView.setImageBitmap(mLeftBitmap);
+                mRightView.setImageBitmap(mRightBitmap);
+                break;
+        }
+    }
+} // end Class
