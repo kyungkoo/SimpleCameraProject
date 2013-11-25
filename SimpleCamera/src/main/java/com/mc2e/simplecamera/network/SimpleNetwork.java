ca42aff15699a2082e0cd86795cc5dd902e16c43
kang kyung koo <kyungkoo.kang@yahoo.com>
2013-11-25 12:32:12 +0900

    networking 을 위한 Library 추가

---
+++ SimpleCamera/src/main/java/com/mc2e/simplecamera/network/SimpleNetwork.java
@@ -0,0 +1,14 @@
+package com.mc2e.simplecamera.network;
+
+import com.loopj.android.http.AsyncHttpClient;
+
+/**
+ * Created by pineone on 11/25/13.
+ */
+public class SimpleNetwork {
+
+    public SimpleNetwork() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+    }
+}
